043f325c5f0a4fac62105ebbe5431070365a3747
HAJIME Fukuna <f97one@gmail.com>
2021-08-16 08:43:02 +0000

    Battery から Input まで追加。

--- app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/InventoryResult.kt
+++ app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/InventoryResult.kt
@@ -3,10 +3,7 @@ package net.formula97.androidapps.sysinfocollector.domain
 import com.tickaroo.tikxml.annotation.Element
 import com.tickaroo.tikxml.annotation.PropertyElement
 import com.tickaroo.tikxml.annotation.Xml
-import net.formula97.androidapps.sysinfocollector.logic.converter.BoolToIntConverter
-import net.formula97.androidapps.sysinfocollector.logic.converter.HexadecimalUuidConverter
-import net.formula97.androidapps.sysinfocollector.logic.converter.StdDateTimeConverter
-import net.formula97.androidapps.sysinfocollector.logic.converter.VTConverter
+import net.formula97.androidapps.sysinfocollector.logic.converter.*
 import java.util.*
 
 @Xml(name = "REQUEST")
@@ -45,7 +42,21 @@ data class InventoryResult(
         @Element
         var operatingSystem: OperatingSystem = OperatingSystem(),
         @Element
-        var antivirus: Antivirus? = null,
+        var antivirus: List<Antivirus> = listOf(),
+        @Element
+        var batteries: List<Battery> = listOf(),
+        @Element
+        var controllers: List<Controller> = listOf(),
+        @Element
+        var cpus: List<Cpu> = listOf(),
+        @Element
+        var drives: List<Drive> = listOf(),
+        @Element
+        var envs: List<Env> = listOf(),
+        @Element
+        var firewall: Firewall? = null,
+        @Element
+        var inputs: List<Input> = listOf(),
         @Element
         var storages: List<Storage> = listOf()
     ) {}
@@ -443,6 +454,322 @@ data class InventoryResult(
          */
         @PropertyElement(name = "BASE_VERSION", writeAsCData = true)
         var BASE_VERSION: String? = null
+    ) {}
+
+    @Xml(name = "BATTERIES")
+    data class Battery(
+        @PropertyElement(name = "CHEMISTRY", writeAsCData = true)
+        var chemistry: String? = null,
+        /**
+         * manufacturing date
+         */
+        @PropertyElement(name = "DATE", writeAsCData = true, converter = StdDateConverter::class)
+        var date: Date? = null,
+        /**
+         * battery name
+         */
+        @PropertyElement(name = "NAME", writeAsCData = true)
+        var name: String = "",
+        /**
+         * battery serial number
+         */
+        @PropertyElement(name = "SERIAL", writeAsCData = true)
+        var serial: String? = null,
+        /**
+         * battery manufacturer
+         */
+        @PropertyElement(name = "MANUFACTURER", writeAsCData = true)
+        var manufacturer: String? = null,
+        /**
+         * battery max capacity (the maximum capacity by design), in mWh
+         */
+        @PropertyElement(name = "CAPACITY", writeAsCData = true)
+        var capacity: Int? = null,
+        /**
+         * battery real max capacity (decreases as the battery gets old), in mWh
+         */
+        @PropertyElement(name = "REAL_CAPACITY", writeAsCData = true)
+        var realCapacity: Int? = null,
+        /**
+         * battery voltage, in mV
+         */
+        @PropertyElement(name = "VOLTAGE", writeAsCData = true)
+        var voltage: Int? = null
+    ) {}
+
+    @Xml(name = "CONTROLLERS")
+    data class Controller(
+        @PropertyElement(name = "DRIVER", writeAsCData = true)
+        var driver: String? = null,
+        /**
+         * device name
+         */
+        @PropertyElement(name = "NAME", writeAsCData = true)
+        var name: String = "",
+        /**
+         * device caption
+         */
+        @PropertyElement(name = "CAPTION", writeAsCData = true)
+        var caption: String = "",
+        /**
+         * manufacturer name
+         */
+        @PropertyElement(name = "MANUFACTURER", writeAsCData = true)
+        var manufacturer: String? = null,
+        /**
+         * device model
+         */
+        @PropertyElement(name = "MODEL", writeAsCData = true)
+        var model: String? = null,
+        /**
+         * device serial number
+         */
+        @PropertyElement(name = "SERIAL", writeAsCData = true)
+        var serial: String? = null,
+        /**
+         * device PCI class ID
+         */
+        @PropertyElement(name = "PCICLASS", writeAsCData = true)
+        var pciClass: String? = null,
+        /**
+         * device PCI vendor ID
+         */
+        @PropertyElement(name = "VENDORID", writeAsCData = true)
+        var vendorId: String? = null,
+        /**
+         * device PCI product ID
+         */
+        @PropertyElement(name = "PRODUCTID", writeAsCData = true)
+        var productId: String? = null,
+        /**
+         * device PCI subsystem ID, ie '8086:2a40'
+         */
+        @PropertyElement(name = "PCISUBSYSTEMID", writeAsCData = true)
+        var pciSubsystemId: String? = null,
+        /**
+         * device PCI slot, ie '00:02.1'
+         */
+        @PropertyElement(name = "PCISLOT", writeAsCData = true)
+        var pciSlot: String? = null,
+        @PropertyElement(name = "TYPE", writeAsCData = true)
+        var type: String? = null,
+        /**
+         * controller revision, ie 'rev 02'
+         */
+        @PropertyElement(name = "REV", writeAsCData = true)
+        var rev: String? = null
+    ) {}
+
+    @Xml(name = "CPUS")
+    data class Cpu(
+        /**
+         * total CPU cache size, in KB
+         */
+        @PropertyElement(name = "CACHE", writeAsCData = true)
+        var cache: Int? = null,
+        /**
+         * number of cores
+         */
+        @PropertyElement(name = "CORE", writeAsCData = true)
+        var core: Int = 1,
+        /**
+         * total available cores, as CORE only reports enabled count
+         */
+        @PropertyElement(name = "CORECOUNT", writeAsCData = true)
+        var coreCount: Int? = null,
+        @PropertyElement(name = "DESCRIPTION", writeAsCData = true)
+        var description: String? = null,
+        /**
+         * CPU manufacturer, ie 'Intel', 'AMD', etc.
+         */
+        @PropertyElement(name = "MANUFACTURER", writeAsCData = true)
+        var manufacturer: String? = null,
+        /**
+         * CPU name, ie 'Intel(R) Core(TM)2 Duo CPU P8600  @ 2.40GHz'
+         */
+        @PropertyElement(name = "NAME", writeAsCData = true)
+        var name: String = "",
+        /**
+         * number of threads per core
+         */
+        @PropertyElement(name = "THREAD", writeAsCData = true)
+        var thread: Int? = null,
+        /**
+         * CPU serial number
+         */
+        @PropertyElement(name = "SERIAL", writeAsCData = true)
+        var serial: String? = null,
+        /**
+         * stepping value
+         */
+        @PropertyElement(name = "STEPPING", writeAsCData = true)
+        var stepping: String? = null,
+        /**
+         * family name
+         */
+        @PropertyElement(name = "FAMILYNAME", writeAsCData = true)
+        var familyName: String? = null,
+        /**
+         * family value
+         */
+        @PropertyElement(name = "FAMILYNUMBER", writeAsCData = true)
+        var familyNumber: String? = null,
+        /**
+         * model value
+         */
+        @PropertyElement(name = "MODEL", writeAsCData = true)
+        var model: String? = null,
+        /**
+         * CPU frequency, in MHz
+         */
+        @PropertyElement(name = "SPEED", writeAsCData = true)
+        var speed: Int? = null,
+        @PropertyElement(name = "ID", writeAsCData = true)
+        var id: String? = null,
+        /**
+         * CPU Architecture
+         */
+        @PropertyElement(name = "ARCH", writeAsCData = true, converter = CpuArchConverter::class)
+        var arch: CpuArch = CpuArch.ARM
+    ) {}
+
+    /**
+     * Filesystem configuration
+     */
+    @Xml(name = "DRIVES")
+    data class Drive(
+        /**
+         * creation date
+         */
+        @PropertyElement(name = "CREATEDATE", writeAsCData = true, converter = StdDateConverter::class)
+        var createDate: Date? = null,
+        @PropertyElement(name = "DESCRIPTION", writeAsCData = true)
+        var description: String = "",
+        /**
+         * free space, in MB
+         */
+        @PropertyElement(name = "FREE", writeAsCData = true)
+        var free: Double? = null,
+        /**
+         * total space, in MB
+         */
+        @PropertyElement(name = "TOTAL", writeAsCData = true)
+        var total: Double? = null,
+        /**
+         * filesystem type, ie 'ext3'
+         */
+        @PropertyElement(name = "FILESYSTEM", writeAsCData = true)
+        var filesystem: String = "",
+        /**
+         * filesystem user name
+         */
+        @PropertyElement(name = "LABEL", writeAsCData = true)
+        var label: String? = null,
+        /**
+         * filesystem system name, ie '/dev/sda1' or 'host:/path' for NFS
+         */
+        @PropertyElement(name = "VOLUME", writeAsCData = true)
+        var volume: String = "",
+        /**
+         * filesystem letter (Windows only)
+         */
+        @PropertyElement(name = "LETTER", writeAsCData = true)
+        var letter: String? = null,
+        /**
+         * filesystem serial number or UUID
+         */
+        @PropertyElement(name = "SERIAL")
+        var serial: String? = null,
+        /**
+         * whether it is the system partition (Windows only)
+         */
+        @PropertyElement(name = "SYSTEMDRIVE", writeAsCData = true, converter = BoolToIntConverter::class)
+        var systemDrive: Boolean = false,
+        /**
+         * filesystem mount point (Unix only)
+         */
+        @PropertyElement(name = "TYPE", writeAsCData = true)
+        var type: String? = null,
+        /**
+         * supported encryption: BitLocker, cryptsetup, FileVault, ...
+         */
+        @PropertyElement(name = "ENCRYPT_NAME", writeAsCData = true)
+        var encryptName: String? = null,
+        /**
+         * encryption algorithm: AES_128, AES_256, HARDWARE_ENCRYPTION, ...
+         */
+        @PropertyElement(name = "ENCRYPT_ALGO", writeAsCData = true)
+        var encryptAlgo: String? = null,
+        /**
+         * encryption status: Yes, No, Partially, ...
+         */
+        @PropertyElement(name = "ENCRYPT_STATUS", writeAsCData = true)
+        var encryptStatus: String? = null,
+        /**
+         * encryption type: hardware, software, ...
+         */
+        @PropertyElement(name = "ENCRYPT_TYPE", writeAsCData = true)
+        var encryptType: String? = null
+    ) {}
+
+    @Xml(name = "ENVS")
+    data class Env(
+        @PropertyElement(name = "KEY", writeAsCData = true)
+        var key: String = "",
+        @PropertyElement(name = "VAL", writeAsCData = true)
+        var value: String? = null
+    ) {}
+
+    /**
+     * Firewall configuration
+     */
+    @Xml(name = "FIREWALL")
+    data class Firewall(
+        /**
+         * active connection's interface (Windows only)
+         */
+        @PropertyElement(name = "DESCRIPTION", writeAsCData = true)
+        var description: String? = null,
+        /**
+         * active connection's ip (Windows only)
+         */
+        @PropertyElement(name = "IPADDRESS", writeAsCData = true)
+        var ipAddress: String? = null,
+        /**
+         * active connection's ip v6 (Windows only)
+         */
+        @PropertyElement(name = "IPADDRESS6", writeAsCData = true)
+        var ipAddress6: String? = null,
+        /**
+         * firewall profile (Windows only)
+         */
+        @PropertyElement(name = "PROFILE", writeAsCData = true)
+        var profile: String? = null,
+        /**
+         * firewall status
+         */
+        @PropertyElement(name = "STATUS", writeAsCData = true)
+        var status: String = ""
+    ) {}
+
+    @Xml(name = "INPUTS")
+    data class Input(
+        @PropertyElement(name = "NAME", writeAsCData = true)
+        var name: String = "",
+        @PropertyElement(name = "MANUFACTURER", writeAsCData = true)
+        var manufacturer: String? = null,
+        @PropertyElement(name = "CAPTION", writeAsCData = true)
+        var caption: String? = null,
+        @PropertyElement(name = "DESCRIPTION", writeAsCData = true)
+        var description: String? = null,
+        @PropertyElement(name = "INTERFACE", writeAsCData = true)
+        var inputInterface: String? = null,
+        @PropertyElement(name = "LAYOUT", writeAsCData = true)
+        var layout: String? = null,
+        @PropertyElement(name = "POINTINGTYPE", writeAsCData = true)
+        var pointingType: String? = null,
+        @PropertyElement(name = "TYPE", writeAsCData = true)
+        var type: String? = null
     ) {}
 
     @Xml(name = "STORAGES")
