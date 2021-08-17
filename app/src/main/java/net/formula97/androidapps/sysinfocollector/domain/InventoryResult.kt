e4357091b37c3e590a5d823e28e43b3b532adbba
HAJIME Fukuna <f97one@gmail.com>
2021-08-17 07:59:21 +0000

    Memory追加。

--- app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/InventoryResult.kt
+++ app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/InventoryResult.kt
@@ -64,6 +64,8 @@ data class InventoryResult(
         @Element
         var loginUsers: List<LocalUser> = listOf(),
         @Element
+        var memories: List<Memory> = listOf(),
+        @Element
         var storages: List<Storage> = listOf()
     ) {}
 
@@ -894,6 +896,54 @@ data class InventoryResult(
          */
         @PropertyElement(name = "PRODUCTID", writeAsCData = true)
         var productId: String? = null
+    ) {}
+
+    @Xml(name = "MEMORIES")
+    data class Memory(
+        /**
+         * memory capacity, in MB
+         */
+        @PropertyElement(name = "CAPACITY", writeAsCData = true)
+        var capacity: Int? = null,
+        @PropertyElement(name = "CAPTION", writeAsCData = true)
+        var caption: String? = null,
+        /**
+         * See Win32_PhysicalMemory documentation
+         */
+        @PropertyElement(name = "FORMFACTOR", writeAsCData = true)
+        var FORM_FACTOR: String? = null,
+        @PropertyElement(name = "REMOVABLE", writeAsCData = true, converter = BoolToIntConverter::class)
+        var removable: Boolean = false,
+        /**
+         * See Win32_PhysicalMemory documentation
+         */
+        @PropertyElement(name = "PURPOSE", writeAsCData = true)
+        var purpose: String? = null,
+        /**
+         * memory frequency, in Mhz
+         */
+        @PropertyElement(name = "SPEED", writeAsCData = true)
+        var speed: Int? = null,
+        /**
+         * memory serial number
+         */
+        @PropertyElement(name = "SERIALNUMBER", writeAsCData = true)
+        var serialNumber: String? = null,
+        /**
+         * memory model
+         */
+        @PropertyElement(name = "MODEL", writeAsCData = true)
+        var model: String? = null,
+        @PropertyElement(name = "TYPE", writeAsCData = true)
+        var type: String? = null,
+        @PropertyElement(name = "DESCRIPTION", writeAsCData = true)
+        var description: String? = null,
+        @PropertyElement(name = "NUMSLOTS", writeAsCData = true)
+        var NUM_SLOTS: Int? = null,
+        @PropertyElement(name = "MEMORYCORRECTION", writeAsCData = true)
+        var memoryCorrection: String? = null,
+        @PropertyElement(name = "MANUFACTURER", writeAsCData = true)
+        var manufacturer: String? = null
     ) {}
 
     @Xml(name = "STORAGES")
