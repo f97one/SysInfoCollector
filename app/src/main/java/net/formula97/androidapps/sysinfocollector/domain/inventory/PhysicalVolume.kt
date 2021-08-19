bd6bd4d39a04e691e9f036d9854b4c1f3dc2f5f3
HAJIME Fukuna <f97one@gmail.com>
2021-08-19 04:59:21 +0000

    LVM物理ボリュームを追加。

---
+++ app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/inventory/PhysicalVolume.kt
@@ -0,0 +1,46 @@
+package net.formula97.androidapps.sysinfocollector.domain.inventory
+
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+import net.formula97.androidapps.sysinfocollector.logic.converter.UuidConverter
+import java.util.*
+
+/**
+ * a LVM physical volume
+ */
+@Xml(name = "PHYSICAL_VOLUMES")
+data class PhysicalVolume(
+    /**
+     * UUID
+     */
+    @PropertyElement(name = "PV_UUID", writeAsCData = true, converter = UuidConverter::class)
+    var pvUuid: UUID? = null,
+    /**
+     * device name, ie '/dev/sda1'
+     */
+    @PropertyElement(name = "DEVICE", writeAsCData = true)
+    var device: String? = null,
+    @PropertyElement(name = "PV_PE_COUNT", writeAsCData = true)
+    var pvPeCount: String?= null,
+    /**
+     * format, ie 'lvm2'
+     */
+    @PropertyElement(name = "FORMAT", writeAsCData = true)
+    var format: String? = null,
+    /**
+     * attributes
+     */
+    @PropertyElement(name = "ATTR")
+    var attr: String? = null,
+    /**
+     * size of physical volume, in MB
+     */
+    @PropertyElement(name = "SIZE", writeAsCData = true)
+    var size: Double? = null,
+    @PropertyElement(name = "FREE", writeAsCData = true)
+    var free: Double? = null,
+    @PropertyElement(name = "PE_SIZE", writeAsCData = true)
+    var peSize: Double? = null,
+    @PropertyElement(name = "VG_UUID", writeAsCData = true)
+    var vgUuid: UUID? = null
+)
