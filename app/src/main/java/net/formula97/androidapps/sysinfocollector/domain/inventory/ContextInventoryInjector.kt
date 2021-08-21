3fddb41f25f30360e4c8ed011e0f82e19c42a7d0
HAJIME Fukuna <f97one@gmail.com>
2021-08-21 17:39:01 +0900

    複数デバイスの場合の考慮漏れを対応。

--- app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/inventory/ContextInventoryInjector.kt
+++ app/src/main/java/net/formula97/androidapps/sysinfocollector/domain/inventory/ContextInventoryInjector.kt
@@ -12,7 +12,15 @@ interface ContextInventoryInjector<T: InventoryCategory> {
      * Gets inventory from [Context] .
      *
      * @param context target Context
-     * @return Inventory object injected from Context
+     * @return Inventory object injected from Context, if device has multiple inventory, returns first one
      */
     fun fromContext(context: Context): T
+
+    /**
+     * Gets inventory as List from [Context] .
+     *
+     * @param context target context
+     * @return List of Inventory objects from Context
+     */
+    fun listFromContext(context: Context): MutableList<T>
 }
